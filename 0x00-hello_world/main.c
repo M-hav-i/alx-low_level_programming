ea429d36007f025e59bd5df1bdd01df6e28bdf76
M-hav-i <deborahmuhavi9@gmail.com>
2022-09-11 03:06:22 -0700

    Preprocessor

---
+++ 0x00-hello_world/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+
+
+/**
+ *
+ *  * main - Entry point
+ *
+ *   *
+ *
+ *    * Return: Always 0 (Success)
+ *
+ *     */
+
+int main(void)
+
+{
+	printf("gcc -E $CFILE - O c\n");
+	    return (0);
+
+}
